8f8aee09a03002dad88850beaaf34d915307bc36
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-20 11:56:56 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- function.php
+++ function.php
@@ -5,7 +5,7 @@
     function data_add($iname, $email, $mobile, $tag){
         $con = Connection();
 
-
+        $tagArray = explode("\n",$tag);
 
         // $insert_data = "INSERT INTO data_tbl(iname,email,moblie,tag,add_at)VALUES('$iname','$email','$mobile','$tag', NOW())";
         // $insert_data_result = mysqli_query($con, $insert_data);
