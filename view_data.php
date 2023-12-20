2199fe5841c4b2c7e86e0f68d7652062e85a3726
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-20 10:57:37 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- view_data.php
+++ view_data.php
@@ -10,6 +10,16 @@
     <br><br>
 
     <form action="" method="post">
-
+        <div class="row">
+            <div class="col-lg-8">
+                <input type="text" name="search_tag" id="" class="form-control">
+            </div>
+            <div class="col-lg-4">
+                <input type="submit" value="Search" name="tag_search" class="btn btn-success" style="width:100%;">
+            </div>
+        </div>
     </form>
+
+    <hr>
+
 </div>
