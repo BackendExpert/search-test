ae27542fbe86573219d81d9a532a5daa02ee29d5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-20 12:35:01 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- function.php
+++ function.php
@@ -5,13 +5,11 @@
     function data_add($iname, $email, $mobile, $tag){
         $con = Connection();
 
-        $tagArray = explode("\n",$tag);
-        $jsonData = json_encode($tagArray);
 
-        // print_r($tagArray);
+        
 
-        $insert_data = "INSERT INTO data_tbl(iname,email,moblie,tag,add_at)VALUES('$iname','$email','$mobile','$jsonData', NOW())";
-        $insert_data_result = mysqli_query($con, $insert_data);
+        // $insert_data = "INSERT INTO data_tbl(iname,email,moblie,tag,add_at)VALUES('$iname','$email','$mobile','$jsonData', NOW())";
+        // $insert_data_result = mysqli_query($con, $insert_data);
     }
 
     function all_data(){
