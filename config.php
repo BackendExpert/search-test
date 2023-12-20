6c7cd969c10fb5ab7033e850c999ede1f5e01076
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-20 10:24:50 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- config.php
+++ config.php
@@ -4,6 +4,11 @@
         $user = "root";
         $pass = "";
         $db_name = "search_test";
+
+        $con = mysqli_connect($server,$user,$pass,$db_name);
+
+        $result = (!$con)?"Connection Lost":$con;
+        return $result;
     }
 
 ?>
