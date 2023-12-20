3b68e5acc166e33d9064d8afb8bb91c53c78cffe
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-20 10:57:57 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- view_data.php
+++ view_data.php
@@ -12,7 +12,7 @@
     <form action="" method="post">
         <div class="row">
             <div class="col-lg-8">
-                <input type="text" name="search_tag" id="" class="form-control">
+                <input type="text" name="search_tag" id="" class="form-control" required>
             </div>
             <div class="col-lg-4">
                 <input type="submit" value="Search" name="tag_search" class="btn btn-success" style="width:100%;">
